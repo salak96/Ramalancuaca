f1eecd7521566d4d4afa6bbaaeb36ce9660337a1
lambang <sasangkakurniawan.com>
2023-04-25 21:30:45 +0700

    adddsx

--- assets/main.js
+++ assets/main.js
@@ -1,23 +1,20 @@
 //consum api from weatherapi-com.p.rapidapi.com
 //https://rapidapi.com/community/api/open-weather-map/endpoints
 
-const getWeatherForecast  = async (cityName) => {
-  try{
-    const response = await fetch(`https://weatherapi-com.p.rapidapi.com/forecast.json?q=${cityName}&days=3`, {
-        method: "GET",
-        headers: {
-          "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
-          "x-rapidapi-key": ProcessingInstruction.env.API_KEY,
-        },
-      });
-      const data = await response.json();
-      console.log(data);
-      return data;
-    } catch (error) {
-      console.log(error);
-      return;
-    }
-  }
+fetch("https://weatherapi-com.p.rapidapi.com/forecast.json?q=${cityName}&days=3", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+		"x-rapidapi-key": process.env.API_KEY // Ini adalah variable environment
+	}
+})
+.then(response => {
+	console.log(response.json());
+})
+.catch(err => {
+	console.error(err);
+});
+
   const displayCityName = (weatherData) => {
     const cityNameDiv = document.getElementById("city-description");
     const cityName = weatherData.location.name;
